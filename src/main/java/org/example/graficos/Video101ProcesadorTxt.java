a071d6f5b1da2bb5336ed62ab86551f0739a43ce
TheDevil <novusordo96@gmail.com>
2025-06-13 10:25:26 -0500

    video101, sin metodos para obtimisar el codigo

--- src/main/java/org/example/graficos/Video101ProcesadorTxt.java
+++ src/main/java/org/example/graficos/Video101ProcesadorTxt.java
@@ -24,58 +24,58 @@ class MarcoProcesador extends JFrame{
 }
 class PanelProcesador extends JPanel{
     public PanelProcesador(){
-     setLayout(new BorderLayout());
-     JPanel subPanel=new JPanel(new BorderLayout());
+        setLayout(new BorderLayout());
+        JPanel subPanel=new JPanel(new BorderLayout());
 
-     JMenuBar barra=new JMenuBar();
-     JMenu fuente=new JMenu("Fuente");
-     JMenu stilo=new JMenu("Estilo");
-     JMenu tamano=new JMenu("Tamaño");
+        JMenuBar barra=new JMenuBar();
+        JMenu fuente=new JMenu("Fuente");
+        JMenu stilo=new JMenu("Estilo");
+        JMenu tamano=new JMenu("Tamaño");
 
-     barra.add(fuente);
-     barra.add(stilo);
-     barra.add(tamano);
+        barra.add(fuente);
+        barra.add(stilo);
+        barra.add(tamano);
 
-     JMenuItem arial =new JMenuItem("Arial");
-     arial.addActionListener(new ActionListener(){
-         public void actionPerformed(ActionEvent e) {
-             area.setFont(new Font("arial",Font.PLAIN,12));
-         }
-     });
-     JMenuItem courier=new JMenuItem("courier");
-     courier.addActionListener(new EventoProcesador());
-     JMenuItem verdana=new JMenuItem("Verdana");
+        JMenuItem arial =new JMenuItem("Arial");
+        arial.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                area.setFont(new Font("arial",Font.PLAIN,12));
+            }
+        });
+        JMenuItem courier=new JMenuItem("courier");
+        courier.addActionListener(new EventoProcesador());
+        JMenuItem verdana=new JMenuItem("Verdana");
         arial.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 area.setFont(new Font("verdana",Font.PLAIN,12));
             }
         });
 
-     JMenuItem negrita=new JMenuItem("negrita");
-     JMenuItem curiva=new JMenuItem("cursica");
+        JMenuItem negrita=new JMenuItem("negrita");
+        JMenuItem curiva=new JMenuItem("cursica");
 
-     JMenuItem doce=new JMenuItem("12");
-     JMenuItem catorce=new JMenuItem("14");
-     JMenuItem diesicies=new JMenuItem("16");
-     JMenuItem diesiocho=new JMenuItem("18");
-     JMenuItem veinte=new JMenuItem("20");
+        JMenuItem doce=new JMenuItem("12");
+        JMenuItem catorce=new JMenuItem("14");
+        JMenuItem diesicies=new JMenuItem("16");
+        JMenuItem diesiocho=new JMenuItem("18");
+        JMenuItem veinte=new JMenuItem("20");
 
-     tamano.add(doce);
-     tamano.add(catorce);
-     tamano.add(diesicies);
-     tamano.add(diesiocho);
-     tamano.add(veinte);
-     stilo.add(negrita);
-     stilo.add(curiva);
-     fuente.add(arial);
-     fuente.add(courier);
-     fuente.add(verdana);
+        tamano.add(doce);
+        tamano.add(catorce);
+        tamano.add(diesicies);
+        tamano.add(diesiocho);
+        tamano.add(veinte);
+        stilo.add(negrita);
+        stilo.add(curiva);
+        fuente.add(arial);
+        fuente.add(courier);
+        fuente.add(verdana);
 
-     add(barra,BorderLayout.NORTH);
+        add(barra,BorderLayout.NORTH);
 
-     area =new JTextPane();
-     subPanel.add(area);
-     add(subPanel,BorderLayout.CENTER);
+        area =new JTextPane();
+        subPanel.add(area);
+        add(subPanel,BorderLayout.CENTER);
     }
     private class EventoProcesador implements ActionListener{
         @Override
